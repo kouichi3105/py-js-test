8066a49131c67b15ff38d48f818e7954000b3f94
kouichi-11” <itinose.ko.1@gmail.com>
2021-02-23 09:27:00 +0900

    flask

--- flask/app/app.py
+++ flask/app/app.py
@@ -3,11 +3,15 @@ from flask import Flask,render_template,request
 from flask_cors import CORS
 
 #Flaskオブジェクトの生成
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../../build/static", template_folder="../../build")
 CORS(app)
 
-#「/」へアクセスがあった場合に、"Hello World"の文字列を返す
+
 @app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/hello")
 def hello():
     return "Hello World"
 
@@ -15,12 +19,5 @@ def hello():
 def data():
     return request.get_data()
 
-#「/index」へアクセスがあった場合に、「index.html」を返す
-@app.route("/api/sample")
-def index():
-    return render_template("index.html")
-
-
-#おまじない
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0', port=5000)
